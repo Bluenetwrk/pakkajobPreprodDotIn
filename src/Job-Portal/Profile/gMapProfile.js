b7237818feb9739fd16f1b26cd14fbf1fd8d5b13
parvaiz <parvaizmahroo1@gmail.com>
2026-08-25 17:29:55 +0530

    new changes

--- src/Job-Portal/Profile/gMapProfile.js
+++ src/Job-Portal/Profile/gMapProfile.js
@@ -1,14 +1,62 @@
-import React from 'react'
+import axios from "axios";
+import React from "react";
+import { useGoogleLogin } from "@react-oauth/google";
 
-function gMapProfile() {
-      let empId = JSON.parse(localStorage.getItem("EmpIdG"))
-      let empGToken = JSON.parse(localStorage.getItem("EmpLog"))
-      console.log("empId", empId, atob(empGToken))
+function GMapProfile() {
+  const empId = JSON.parse(localStorage.getItem("EmpIdG"));
+  const empGToken = JSON.parse(localStorage.getItem("EmpLog"));
+  const gToken = atob(empGToken);
+
+  const login = useGoogleLogin({
+    scope: "https://www.googleapis.com/auth/business.manage",
+
+    onSuccess: async (response) => {
+      try {
+        const gtoken = response.access_token;
+
+        console.log("OAuth response:", response);
+
+        // Get Google user information
+        const user = await axios.get(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers: {
+              Authorization: `Bearer ${gtoken}`,
+            },
+          }
+        );
+
+        console.log("USER:", user.data);
+
+        // Get Google Business Profile accounts
+        const accounts = await axios.get(
+          "https://mybusinessaccountmanagement.googleapis.com/v1/accounts",
+          {
+            headers: {
+              Authorization: `Bearer ${gtoken}`,
+            },
+          }
+        );
+
+        console.log("BUSINESS ACCOUNTS:", accounts.data);
+      } catch (error) {
+        console.log("STATUS:", error.response?.status);
+        console.log("ERROR:", error.response?.data);
+      }
+    },
+
+    onError: (error) => {
+      console.log("Google login error:", error);
+    },
+  });
+
   return (
-    <div style={{ margin: '10px' }}>
-      gMapProfile
+    <div style={{ margin: "10px" }}>
+      <button onClick={() => login()}>
+        Connect Google Business
+      </button>
     </div>
-  )
+  );
 }
 
-export default gMapProfile
+export default GMapProfile;
